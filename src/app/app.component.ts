3aae682f7d6ff56cd26df578bd821b643507c69f
David_Randall <oakesrandall@gmail.com>
2017-08-16 07:58:58 -0600

    got update working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
 
+class quote {
+    commentText: string;
+    author: string;
+    showEdit: boolean = false;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
-  ];
+    updateCommentText;
+    updateAuthor;
+    commentText;
+    author;
+    newQuote: quote;
+    
+    constructor(){
+        this.newQuote = new quote(); 
+    };
+
+    comments = [{
+          showEdit: false,
+          commentText: 'first comment!',
+          author: 'Joe',
+      }, {
+          showEdit: false,
+          commentText: 'nice work!',
+          author: 'Frank',  
+      }, {
+          showEdit: false,
+          commentText: 'I would also like to congratulate you!',
+          author: 'Mary',       
+    }];
+
+  addComment() {
+      this.newQuote.commentText = this.commentText;
+      this.newQuote.author = this.author;
+      this.newQuote.showEdit = false;
+      this.comments.push(this.newQuote);
+      console.log(this.comments);
+  }
+
+  deleteComment(index) {
+      console.log(index);
+      this.comments.splice(index, 1);
+  }
+
+  showUpdate(index) {
+      this.comments[index].showEdit = true;
+      console.log('running showEdit, it is now ' + this.comments[index].showEdit);
+  }
+
+  updateComment(index) {
+          this.comments[index].commentText = this.updateCommentText;
+          this.comments[index].author = this.updateAuthor;
+          this.comments[index].showEdit = false;
+          console.log(this.comments);
+  }
 }
+
+
+
---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs/Observable';
+
+// @Injectable()
+// export class CommentService {
+
+//   constructor() { }
+
+//   addComment() => {
+//       this.tempObj.text = this.commentText;
+//       console.log(this.commentText);
+//   }
+
+// }
