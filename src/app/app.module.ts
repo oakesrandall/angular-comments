0b2a8b5cc4b0094282004663812a93d5801bdd25
David_Randall <oakesrandall@gmail.com>
2017-08-17 10:06:52 -0600

    finally got materialize to work with angular, applied basic button styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+// import { MaterializeModule } from '../src/index'
 
 import { AppComponent } from './app.component';
 // import { CommentsComponent } from './comments/comments.component';
